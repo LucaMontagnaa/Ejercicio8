0a7ffca9ee1a1dea9e8c27dce37bbecb54b3086e
LucaMontagnaa <101721205+LucaMontagnaa@users.noreply.github.com>
2022-04-10 14:49:33 -0300

    Ejercicio7 completed

--- Assets/Scripts/Ejercicio8.cs
+++ Assets/Scripts/Ejercicio8.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Ejercicio8 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int diaSemana;
     void Start()
     {
-        
+        if (diaSemana > 7)
+        {
+            Debug.Log(" El día ingresado no es válido");
+        }
+        else if (diaSemana <= 0)
+        {
+            Debug.Log(" El día ingresado no es válido");
+        }
+       else if (diaSemana == 1)
+        {
+            Debug.Log("El día ingresasdo corresponde al lunes");
+        }
+        else if (diaSemana == 2)
+        {
+            Debug.Log("El día ingresasdo corresponde al martes");
+        }
+        else if (diaSemana == 3)
+        {
+            Debug.Log("El día ingresasdo corresponde al miercoles");
+        }
+        else if (diaSemana == 4)
+        {
+            Debug.Log("El día ingresasdo corresponde al jueves");
+        }
+        else if (diaSemana == 5)
+        {
+            Debug.Log("El día ingresasdo corresponde al viernes");
+        }
+        else if (diaSemana == 6)
+        {
+            Debug.Log("El día ingresasdo corresponde al sabado");
+        }
+        else if (diaSemana == 7)
+        {
+            Debug.Log("El día ingresasdo corresponde al domingo");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
